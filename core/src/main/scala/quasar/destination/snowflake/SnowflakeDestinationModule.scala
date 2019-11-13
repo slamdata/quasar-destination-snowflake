7d674050e00bc92a914f177cdb610bb863fd0495
Juan Pablo Santos <santos.juanpablo@gmail.com>
2019-11-13 17:30:02 -0500

    Implement a liveness check for Snowflake

--- core/src/main/scala/quasar/destination/snowflake/SnowflakeDestinationModule.scala
+++ core/src/main/scala/quasar/destination/snowflake/SnowflakeDestinationModule.scala
@@ -24,6 +24,7 @@ import quasar.api.destination.{Destination, DestinationError, DestinationType}
 import quasar.connector.{DestinationModule, MonadResourceErr}
 import quasar.concurrent.NamedDaemonThreadFactory
 
+import java.sql.SQLException
 import java.util.concurrent.Executors
 
 import scala.concurrent.ExecutionContext
@@ -31,12 +32,18 @@ import scala.util.{Either, Random}
 
 import argonaut._, Argonaut._
 
+import cats.MonadError
 import cats.data.EitherT
 import cats.effect._
+import cats.implicits._
 
+import doobie._
+import doobie.implicits._
 import doobie.hikari.HikariTransactor
 
 import eu.timepit.refined.auto._
+
+import scalaz.NonEmptyList
 
 object SnowflakeDestinationModule extends DestinationModule {
   def destinationType: DestinationType =
@@ -53,23 +60,59 @@ object SnowflakeDestinationModule extends DestinationModule {
   def destination[F[_]: ConcurrentEffect: ContextShift: MonadResourceErr: Timer](
     config: Json): Resource[F, Either[InitializationError[Json], Destination[F]]] =
     (for {
-      config <- EitherT.fromEither[Resource[F, ?]](config.as[SnowflakeConfig].result) leftMap {
+      cfg <- EitherT.fromEither[Resource[F, ?]](config.as[SnowflakeConfig].result) leftMap {
         case (err, _) => DestinationError.malformedConfiguration((destinationType, config, err))
       }
       poolSuffix <- EitherT.right(Resource.liftF(Sync[F].delay(Random.alphanumeric.take(5).mkString)))
-      connectPool <- EitherT.right(boundedPool(s"snowflake-dest-connect-$poolSuffix", PoolSize))
-      transactPool <- EitherT.right(unboundedPool(s"snowflake-dest-transact-$poolSuffix"))
-      jdbcUri = SnowflakeConfig.configToUri(config)
+      connectPool <- EitherT.right(boundedPool[F](s"snowflake-dest-connect-$poolSuffix", PoolSize))
+      transactPool <- EitherT.right(unboundedPool[F](s"snowflake-dest-transact-$poolSuffix"))
+
+      jdbcUri = SnowflakeConfig.configToUri(cfg)
+
       transactor <- EitherT.right[InitializationError[Json]](
-        HikariTransactor.newHikariTransactor(
+        HikariTransactor.newHikariTransactor[F](
           SnowflakeDriverFqcn,
           jdbcUri,
-          config.user.value,
-          config.password.value,
+          cfg.user.value,
+          cfg.password.value,
           connectPool,
           transactPool))
+
+      _ <- isLive(transactor, config)
+
       destination: Destination[F] = new SnowflakeDestination[F](transactor)
     } yield destination).value
+
+  private def isLive[F[_]: Sync](xa: Transactor[F], config: Json)
+      : EitherT[Resource[F, ?], InitializationError[Json], Unit] = {
+    val MER = MonadError[Resource[F, ?], Throwable]
+
+    MER.attemptT(fr0"SELECT current_version()"
+      .query[Unit].stream.transact(xa).compile.resource.drain) leftSemiflatMap {
+
+      case (ex: SQLException) if ex.getErrorCode === ErrorCode.AccessDenied =>
+        DestinationError.accessDenied(
+          (destinationType, config, ex.getSQLState)).pure[Resource[F, ?]]
+
+      case (ex: SQLException) if ex.getErrorCode === ErrorCode.ConnectionFailed =>
+        DestinationError.connectionFailed(
+          (destinationType, config, ex)).pure[Resource[F, ?]]
+
+      case (ex: SQLException) =>
+        DestinationError.invalidConfiguration(
+          (destinationType,
+            config,
+            NonEmptyList(ex.getMessage, ex.getErrorCode.toString, ex.getSQLState)))
+          .pure[Resource[F, ?]]
+
+      case ex => MER.raiseError(ex)
+    }
+  }
+
+  private object ErrorCode {
+    val AccessDenied: Int = 390100
+    val ConnectionFailed: Int = 200015
+  }
 
   private def boundedPool[F[_]: Sync](name: String, threadCount: Int): Resource[F, ExecutionContext] =
     Resource.make(
