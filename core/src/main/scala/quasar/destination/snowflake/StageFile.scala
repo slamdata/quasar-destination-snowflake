f48a96f41f09d9ae42e31f1d2d59004e90f7f8d9
Maxim Zimaliev <zimaliev@gmail.com>
2021-05-03 15:37:43 +0300

    ...

--- core/src/main/scala/quasar/destination/snowflake/StageFile.scala
+++ core/src/main/scala/quasar/destination/snowflake/StageFile.scala
@@ -19,12 +19,15 @@ package quasar.destination.snowflake
 import slamdata.Predef._
 
 import cats.effect._
+import cats.effect.concurrent.{Ref, Semaphore}
 import cats.implicits._
 
 import doobie._
 import doobie.implicits._
 
 import fs2._
+import fs2.concurrent.Queue
+import fs2.io
 
 import java.io.ByteArrayInputStream
 import java.util.UUID
@@ -32,13 +35,68 @@ import net.snowflake.client.jdbc.SnowflakeConnection
 import org.slf4s.Logger
 import java.nio.charset.StandardCharsets
 
-sealed trait StageFile {
-  def fragment: Fragment
+sealed trait StageFile[F[_]] {
+  def ingest(chunk: Chunk[Byte]): F[Unit]
+  def done: Resource[F, Fragment]
 }
 
 object StageFile {
   private val Compressed = true
 
+  private final case class StageFileState[F[_]](
+      q: Queue[F, Option[Chunk[Byte]]],
+      name: String)
+
+  def apply[F[_]: ConcurrentEffect: ContextShift](
+      xa: Transactor[F],
+      connection: SnowflakeConnection,
+      blocker: Blocker,
+      logger: Logger)
+      : F[StageFile[F]] = {
+    val debug = (s: String) => Sync[F].delay(logger.debug(s))
+
+    for {
+      rq <- Ref.of[F, Option[StageFileState[F]]](None)
+      semaphore <- Semaphore[F](1)
+    } yield {
+      def getOrStart: F[StageFileState[F]] = rq.get flatMap {
+        case Some(q) => q.pure[F]
+        case None => for {
+          q <- Queue.unbounded[F, Option[Chunk[Byte]]]
+          unique <- Sync[F].delay(UUID.randomUUID.toString)
+          name = s"precog_$unique"
+          state = StageFileState(q, name)
+          _ <- semaphore.withPermit {
+            rq.set(state.some) >>
+            ConcurrentEffect[F].start {
+              debug(s"Starting staging to file: @~/$name") >>
+              io.toInputStreamResource(q.dequeue.unNoneTerminate.flatMap(Stream.chunk(_))).use({ is =>
+                blocker.delay[F, Unit](connection.uploadStream("@~", "/", is, name, Compressed))
+              }) >>
+              debug(s"Finished staging to file: @~/$name")
+            }
+          }
+        } yield state
+      }
+      new StageFile[F] {
+        def ingest(c: Chunk[Byte]): F[Unit] =
+          getOrStart.flatMap(_.q.enqueue1(c.some))
+        def done: Resource[F, Fragment] = Resource.liftF(getOrStart) flatMap { state =>
+          val acquire =
+            state.q.enqueue1(None) >>
+            semaphore.withPermit(rq.set(None)) as
+            Fragment.const0(state.name)
+          val release: Fragment => F[Unit] = sf => {
+            val fragment = fr0"rm @~/" ++ sf
+            debug("Cleaning staging file @~/$name up") >>
+            fragment.query[Unit].option.void.transact(xa)
+          }
+          Resource.make(acquire)(release)
+        }
+      }
+    }
+  }
+/*
   def apply(input: Chunk[Byte], connection: SnowflakeConnection, blocker: Blocker, logger: Logger)
       : Resource[ConnectionIO, StageFile] = {
     val bytes = input.toBytes.values
@@ -53,9 +111,7 @@ object StageFile {
       _ <- debug(msg)
       unique <- Sync[ConnectionIO].delay(UUID.randomUUID.toString)
       name = s"precog-$unique"
-      _ <- debug(s"Starting staging to file: @~/$name")
       _ <- blocker.delay[ConnectionIO, Unit](connection.uploadStream("@~", "/", inputStream, name, Compressed))
-      _ <- debug(s"Finished staging to file: @~/$name")
     } yield new StageFile {
       def fragment = Fragment.const0(name)
     }
@@ -68,4 +124,5 @@ object StageFile {
 
     Resource.make(acquire)(release)
   }
+  */
 }
