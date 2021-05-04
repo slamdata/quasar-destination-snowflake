d53ecb11423cc0934d505371c7c2e4eb1b0b8b1d
Maxim Zimaliev <zimaliev@gmail.com>
2021-05-04 17:07:47 +0300

    one deferred instead of two semaphores

--- core/src/main/scala/quasar/destination/snowflake/StageFile.scala
+++ core/src/main/scala/quasar/destination/snowflake/StageFile.scala
@@ -19,7 +19,7 @@ package quasar.destination.snowflake
 import slamdata.Predef._
 
 import cats.effect._
-import cats.effect.concurrent.{Ref, Semaphore}
+import cats.effect.concurrent.{Ref, Deferred}
 import cats.implicits._
 
 import doobie._
@@ -43,8 +43,10 @@ object StageFile {
 
   private final case class StageFileState[F[_]](
       q: Queue[F, Option[Chunk[Byte]]],
+      isDone: Deferred[F, Unit],
       name: String)
 
+  // Thread unsafe, `ingest` and `done` shouldn't be call in parallel
   def apply[F[_]: ConcurrentEffect: ContextShift](
       xa: Transactor[F],
       connection: SnowflakeConnection,
@@ -55,11 +57,6 @@ object StageFile {
 
     for {
       rq <- Ref.of[F, Option[StageFileState[F]]](None)
-      // This is acquired by uploaind process, ensuring that we don't do anything in `done` until
-      // stage file is uploaded
-      doneSemaphore <- Semaphore[F](1)
-      // This ensures that we call only one ingest or done simultaneously
-      uniqueSemaphore <- Semaphore[F](1)
     } yield {
       def getOrStart: F[StageFileState[F]] = rq.get flatMap {
         case Some(q) => q.pure[F]
@@ -67,44 +64,37 @@ object StageFile {
           q <- Queue.unbounded[F, Option[Chunk[Byte]]]
           unique <- Sync[F].delay(UUID.randomUUID.toString)
           name = s"precog_$unique"
-          state = StageFileState(q, name)
+          doneDeferred <- Deferred[F, Unit]
+          state = StageFileState(q, doneDeferred, name)
           _ <- rq.set(state.some)
           _ <- ConcurrentEffect[F].start {
-            doneSemaphore.withPermit {
-              debug(s"Starting staging to file: @~/$name") >>
-              io.toInputStreamResource(q.dequeue.unNoneTerminate.flatMap(Stream.chunk(_))).use({ is =>
-                blocker.delay[F, Unit](connection.uploadStream("@~", "/", is, name, Compressed))
-              }) >>
-              debug(s"Finished staging to file: @~/$name")
-            }
+            debug(s"Starting staging to file: @~/$name") >>
+            io.toInputStreamResource(q.dequeue.unNoneTerminate.flatMap(Stream.chunk(_))).use({ is =>
+              blocker.delay[F, Unit](connection.uploadStream("@~", "/", is, name, Compressed))
+            }) >>
+            doneDeferred.complete(()) >>
+            debug(s"Finished staging to file: @~/$name")
           }
         } yield state
       }
-      val uniqueSemaphoreR: Resource[F, Unit] =
-        Resource.make(uniqueSemaphore.acquire)(x => uniqueSemaphore.release)
-
       new StageFile[F] {
-        def ingest(c: Chunk[Byte]): F[Unit] = uniqueSemaphore withPermit {
+        def ingest(c: Chunk[Byte]): F[Unit] =
           getOrStart.flatMap(_.q.enqueue1(c.some))
-        }
-        def done: Resource[F, Option[Fragment]] = {
-          (uniqueSemaphoreR >> Resource.liftF(rq.get)) flatMap { _.traverse { state =>
-            val acquire =
-              state.q.enqueue1(None) >>
-              doneSemaphore.acquire >>
-              rq.set(None) as
-              Fragment.const0(state.name)
 
-            val release: Fragment => F[Unit] = sf => {
+        def done: Resource[F, Option[Fragment]] = Resource.liftF(rq.get) flatMap { _.traverse { state =>
+          val acquire =
+            state.q.enqueue1(None) >>
+            state.isDone.get >>
+            rq.set(None) as
+            Fragment.const0(state.name)
 
-              val fragment = fr0"rm @~/" ++ sf
-              debug("Cleaning staging file @~/$name up") >>
-              fragment.query[Unit].option.void.transact(xa) >>
-              doneSemaphore.release
-            }
-            Resource.make(acquire)(release)
-          }}
-        }
+          val release: Fragment => F[Unit] = sf => {
+            val fragment = fr0"rm @~/" ++ sf
+            debug("Cleaning staging file @~/$name up") >>
+            fragment.query[Unit].option.void.transact(xa)
+          }
+          Resource.make(acquire)(release)
+        }}
       }
     }
   }
